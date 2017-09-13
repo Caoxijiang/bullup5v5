d70fda9870f17abc480c865df0e75a9d5236db6a
Git-Wangchong <907634055@qq.com>
2017-09-13 13:50:36 +0800

    优化

--- SPAFront/js/chatroomrouting.js
+++ SPAFront/js/chatroomrouting.js
@@ -1,10 +1,10 @@
 
 $().ready(function(){
     $('#router_chat').on('click', function(e){
-       
+      
         e.preventDefault();
-        bullup.loadTemplateIntoTarget('swig_playbyplay.html', {}, 'main-view');
-        $.getScript('/js/playly.js');
+        bullup.loadTemplateIntoTarget('chatroom.html', {}, 'main-view');
+        // $.getScript('/js/playly.js');
       
         
 	    });
